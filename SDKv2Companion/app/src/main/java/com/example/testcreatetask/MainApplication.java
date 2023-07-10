1cb6ff2ac0b51ff3ef7704134ea8175221f97ae1
kho <kho@bluefrogrobotics.com>
2023-07-10 12:39:26 +0200

    template to launch mission from SDK

---
+++ SDKv2Companion/app/src/main/java/com/example/testcreatetask/MainApplication.java
@@ -0,0 +1,10 @@
+package com.example.testcreatetask;
+
+import com.bfr.buddysdk.BuddyApplication;
+
+public class MainApplication extends BuddyApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
