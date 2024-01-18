f28440f773ad94fdfd3b5fad88dcc597c43976b6
Ilya S <ilya.s@fmlht.com>
2024-01-18 19:09:48 +0100

    Add Notifications demo app

---
+++ SDKv2Notifications/app/src/main/java/com/bfr/sdkv2_notifications/DemoApplication.java
@@ -0,0 +1,10 @@
+package com.bfr.sdkv2_notifications;
+
+import com.bfr.buddysdk.BuddyApplication;
+
+public class DemoApplication extends BuddyApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
